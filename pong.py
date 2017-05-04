2fb5dc57779e92ba06827c27fcd6327dfcbe6a5b
Your Name <you@example.com>
2017-05-04 02:32:04 +0000

    m

--- pong.py
+++ pong.py
@@ -8,63 +8,67 @@ pygame.display.set_caption('Animation')
 red=(255,0,0)
 x=300
 y=300
+
+xc=random.randint(1,10)
+yc=random.randint(1,10)
+v=0
+vc=500
+lc=0
+l=500
 fpsclock=pygame.time.Clock()
 green=(0,255,0)
 blue=(0,0,255)
 white=(255,255,255)
 black=(0,0,0)
 color=(255,0,0)
-fx=0
-fy=0
-xc=random.randint(1,10)
-yc=random.randint(1,10)
-br=0
-dr=0
-snowlist=[]
-for i in range(100):
-    a=[random.randint(0,800), random.randint(0,800)]
-    snowlist.append(a)
-
-
-    
-
-                
 while True:
-    fpsclock.tick(35)
-
     pygame.display.update()
-
-    
-    screen.fill(black)
-    for sdf in range(100):
-        pygame.draw.circle(screen,dr,(snowlist[sdf]),2)
-        snowlist[sdf][1]=snowlist[sdf][1]+1
-        if snowlist[sdf][1]==800:
-            snowlist[sdf][1]=random.randint(0,800)
-            snowlist[sdf][0]=random.randint(0,800)
-        r=random.randint(10,255)
-        g=random.randint(10,255)
-        b=random.randint(10,255)
-        dr=(r,g,b)
-       
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            exit()
+        if event.type==KEYDOWN:
+             if event.key == K_l:
+                 lc=2
+             if event.key == K_o:
+                 lc=-2
+             if event.key == K_s:
+                 v=2
+             if event.key == K_w:
+                 v=-2
+        
+        if event.type==KEYUP:
+             if  event.key==K_l:
+                 lc=0
+             if event.key==K_o:
+                 lc=0
+             if  event.key==K_s:
+                 v=0
+             if event.key==K_w:
+                 v=0
                 
+    vc=vc+v
+    l=l+lc          
+
+    screen.fill(black)
+    pygame.draw.circle(screen,blue,(x,y),50)
+    x+=xc
+    y+=yc
+    if x>=750:
+       xc=-random.randint(1,2)
+    if y>=750:
+        yc=-random.randint(1,2)
+    if x<=0:
+        xc=random.randint(1,2)
+    if y<=0:
+       yc=random.randint(1,2)
+    pygame.draw.rect(screen,red,(800,l,100,200))
+    pygame.draw.rect(screen,red,(0,vc,100,200))
    
+   
+    
 
-
-
-##while True:
-##    pygame.display.update()
-##    screen.fill(black)
-##    pygame.draw.circle(screen,blue,(x,y),50)
-##    x+=xc
-##    y+=yc
-##    if x>=750:
-##        xc=-random.randint(1,5)
-##    if y>=550:
-##        yc=-random.randint(1,5)
-##    if x<=0:
-##        xc=random.randint(1,5)
-##    if y<=0:
-##        yc=random.randint(1,5)
+        
+        
 
 
